18a52648bcef68c657c69347f9cc3e93587e2fd9
sehrishbibi01-CS <sehrish.biibi01@gmail.com>
2026-07-22 21:54:56 +0500

    add file 33 to 36

---
+++ file33.js
@@ -0,0 +1,22 @@
+// OBJECTS
+
+const person= {
+name:"sehrish",
+age:21,
+hobbies:["crafts", "arts","painting"]
+}
+
+// DOT
+// console.log (person);
+// console.log(person.name);
+// console.log(person.age);
+// console.log(person.hobbies);
+
+// BRACKET
+console.log(person["name"])
+console.log(person["age"])
+console.log(["hobbies"])
+ 
+// TO ADD KEY VALUE
+person.gender="female";
+console.log(person)
---
+++ file34.js
@@ -0,0 +1,10 @@
+// ITERATING OBJECT (TO GET ONLY KEY)
+const person={
+    name:"sehrish",
+    age:21,
+    "person hobbies":["arts", "crafts", "guitar"]
+}
+for(let key in person){
+    console.log(key);
+
+}
